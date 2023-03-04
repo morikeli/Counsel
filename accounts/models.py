b7a376d6f2de37bfaa170d99e19904e3bfda3653
brocode <kelimbth@gmail.com>
2023-03-04 16:09:49 +0300

    added function to resize profile pictures

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from PIL import Image
 
 
 class PatientsProfile(models.Model):
@@ -22,6 +22,15 @@ class PatientsProfile(models.Model):
     def __str__(self):
         return f'{self.patient}'
 
+    def save(self, *args, **kwargs):
+        super(PatientsProfile, self).save(*args, **kwargs)
+
+        dp = Image.open(self.profile_pic.path)
+        if dp.height > 500 and dp.width > 500:
+            output_size = (500, 500)
+            dp.thumbnail(output_size)
+            dp.save(self.profile_pic.path)
+
 
 class CounsellorsProfile(models.Model):
     id  = models.CharField(max_length=18, primary_key=True, unique=True, editable=False)
@@ -30,7 +39,7 @@ class CounsellorsProfile(models.Model):
     phone_no = models.CharField(max_length=14, blank=False)
     age = models.PositiveIntegerField(default=0, editable=False)
     dob = models.DateField(blank=False)
-    profile_pic = models.ImageField(upload_to='Patients-Dps/', default='default.png')
+    profile_pic = models.ImageField(upload_to='Counsellors-Dps/', default='default.png')
     marital_status = models.CharField(max_length=30, blank=False)
     is_counsellor = models.BooleanField(default=False, editable=False)
     created = models.DateTimeField(auto_now_add=True)
@@ -42,4 +51,13 @@ class CounsellorsProfile(models.Model):
 
     def __str__(self):
         return f'{self.counsellor}'
+
+    def save(self, *args, **kwargs):
+        super(CounsellorsProfile, self).save(*args, **kwargs)
+
+        dp = Image.open(self.profile_pic.path)
+        if dp.height > 500 and dp.width > 500:
+            output_size = (500, 500)
+            dp.thumbnail(output_size)
+            dp.save(self.profile_pic.path)
 
