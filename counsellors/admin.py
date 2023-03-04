6034bb6683e7f6fd586e17e8891d52a4576e0f11
brocode <kelimbth@gmail.com>
2023-03-04 15:30:59 +0300

    deleted initial project folders

--- counsellors/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from counsellors.models import CounsellorProfile, WorkProfile
-
-admin.site.register(CounsellorProfile)
-admin.site.register(WorkProfile)
--- users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Feedback, UserProfile, YourStory, AskQuestion, UserAppointment
-
-admin.site.register(UserProfile)
-admin.site.register(YourStory)
-admin.site.register(AskQuestion)
-admin.site.register(UserAppointment)
-admin.site.register(Feedback)
