704263bde018fd19dc10ab74691612aaef73038a
brocode <kelimbth@gmail.com>
2023-03-04 14:23:15 +0300

    created a signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
+class PatientsSignupForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
