cae827117f43d8fe2fc5a0d7310534c572c843ae
brocode <kelimbth@gmail.com>
2023-03-04 14:55:09 +0300

    added logout url path

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login_view, name='user_login'),
     path('create-account/', views.signup_view, name='signup'),
     path('create-staff-account/', views.counsellor_signup_view, name='counsellor_signup'),
+    path('logout/', views.LogoutView, name='logout'),
 
 ]
