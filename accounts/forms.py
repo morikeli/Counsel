4b00f0ea95f4fccb403d04f1cafea824173061de
brocode <kelimbth@gmail.com>
2023-03-04 14:48:57 +0300

    added signup form for counsellors

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,12 @@ class PatientsSignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+class CounsellorsSignupForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
