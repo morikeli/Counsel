191304f377a9a815a2f18615da661846a13b24c8
brocode <kelimbth@gmail.com>
2023-03-04 15:29:41 +0300

    updated media_root

--- src/settings.py
+++ src/settings.py
@@ -114,7 +114,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = 'MediaFiles/'
-MEDIA_ROOT = BASE_DIR,'users-media-files/'
+MEDIA_ROOT = BASE_DIR,'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
