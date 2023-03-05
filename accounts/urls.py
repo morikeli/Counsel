fedf05666434241e53d11387f747412abb44ece6
brocode <kelimbth@gmail.com>
2023-03-05 16:37:41 +0300

    added URL paths to update users profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('login/', views.login_view, name='user_login'),
     path('create-account/', views.signup_view, name='signup'),
     path('create-staff-account/', views.counsellor_signup_view, name='counsellor_signup'),
+    path('profile/<str:patient_name>/view-profile/', views.patientsprofile_view, name='patient_profile'),
+    path('profile/<str:medic_name>/therapist/', views.therapistprofile_view, name='therapist_profile'),
+
     path('logout/', views.LogoutView.as_view(), name='logout'),
 
 ]
