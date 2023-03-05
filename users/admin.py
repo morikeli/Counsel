e88316d769f55c702c09284e8f731489776d2649
brocode <kelimbth@gmail.com>
2023-03-05 17:05:45 +0300

    registered models in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
+from .models import Appointments, Facilities
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Appointments)
+class FacilitiesTable(admin.ModelAdmin):
+    list_display = ['medic', 'facility_name', 'location', 'role', 'opening_hours', 'closing_hours']
+    readonly_fields = ['medic', 'facility_name', 'location', 'role', 'mobile_no', 'opening_hours', 'closing_hours']
+
+
+@admin.register(Facilities)
+class AppointmentsTable(admin.ModelAdmin):
+    list_display = ['name', 'therapist_name', 'session', 'approved']
+    readonly_fields = ['name', 'therapist_name', 'appointment_date', 'appointment_time', 'session', 'approved']
+
