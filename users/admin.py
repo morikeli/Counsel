a352b67b9763ecbb453973141bdde2b981253055
brocode <kelimbth@gmail.com>
2023-03-05 17:50:27 +0300

    fixed SystemCheckError-s

--- users/admin.py
+++ users/admin.py
@@ -2,13 +2,13 @@ from .models import Appointments, Facilities
 from django.contrib import admin
 
 
-@admin.register(Appointments)
+@admin.register(Facilities)
 class FacilitiesTable(admin.ModelAdmin):
     list_display = ['medic', 'facility_name', 'location', 'role', 'opening_hours', 'closing_hours']
     readonly_fields = ['medic', 'facility_name', 'location', 'role', 'mobile_no', 'opening_hours', 'closing_hours']
 
 
-@admin.register(Facilities)
+@admin.register(Appointments)
 class AppointmentsTable(admin.ModelAdmin):
     list_display = ['name', 'therapist_name', 'session', 'approved']
     readonly_fields = ['name', 'therapist_name', 'appointment_date', 'appointment_time', 'session', 'approved']
