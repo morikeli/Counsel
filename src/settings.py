0a5c2cbbec367fd47e8d0bb617c2c2807525f2dd
brocode <kelimbth@gmail.com>
2023-03-06 13:19:32 +0300

    added message_tags config

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+from django.contrib.messages import constants as messages
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,3 +121,7 @@ MEDIA_ROOT = BASE_DIR,'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
