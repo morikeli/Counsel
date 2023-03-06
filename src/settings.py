b506e0c8df07cdbbe132024cb69c9393a6f93e5f
brocode <kelimbth@gmail.com>
2023-03-06 14:15:48 +0300

    fixed TypeError generated by invalid MEDIA_ROOT path config.

--- src/settings.py
+++ src/settings.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = 'MediaFiles/'
-MEDIA_ROOT = BASE_DIR,'media/'
+MEDIA_ROOT = BASE_DIR/'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
