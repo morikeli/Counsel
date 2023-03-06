99a9d13bfa55a38130c7fe3b8483c3cc0771ae0f
brocode <kelimbth@gmail.com>
2023-03-06 12:17:28 +0300

    added new views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ScheduleAppointmentsForm
 
-# Create your views here.
+
+# views to handle patient requests
+
+def patient_homepage_view(request):
+
+    context = {}
+    return render(request, 'users/', context)
+
+
+
+def schedule_appointments_view(request):
+    form = ScheduleAppointmentsForm()
+
+    if request.method == 'POST':
+        form = ScheduleAppointmentsForm(request.POST)
+        if form.is_valid():
+            patient = forms.save(commit=False)
+            patient.name = request.user.patientsprofile
+            patient.save()
+
+            message.success(request, 'Appointment schedule successfully!')
+            return redirect('')
+
+
+    context = {'ScheduleAppointmentForm': form}
+    return render(request, 'users/', context)
+
+# views to handle therapists requests
