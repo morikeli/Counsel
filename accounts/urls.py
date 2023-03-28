aebbe86c0e45e6802cdf09e2bc01fbe11bede714
brocode <kelimbth@gmail.com>
2023-03-28 12:33:48 +0300

    updated URL paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='user_login'),
     path('create-account/', views.signup_view, name='signup'),
-    path('create-staff-account/', views.counsellor_signup_view, name='counsellor_signup'),
-    path('profile/<str:patient_name>/view-profile/', views.patientsprofile_view, name='patient_profile'),
-    path('profile/<str:medic_name>/therapist/', views.therapistprofile_view, name='therapist_profile'),
+    path('profile/<str:name>/view-profile/', views.patientsprofile_view, name='patient_profile'),
 
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
 
