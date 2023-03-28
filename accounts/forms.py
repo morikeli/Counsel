01c44792055c980c953228cf0c4626fac4fc1827
brocode <kelimbth@gmail.com>
2023-03-28 12:29:46 +0300

    updated forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,38 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import PatientsProfile, TherapistsProfile
+from .models import User
 from django import forms
 
-# custom forms for patients
-class PatientsSignupForm(UserCreationForm):
+
+class SignupForm(UserCreationForm):
+    SELECT_GENDER = (
+        (None, '-- Select gender --'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    SELECT_MARITAL_STATUS = (
+        (None, '-- Select your marital status --'),
+        ('Single', 'Single'),
+        ('Engaged', 'Engaged'),
+        ('Married', 'Married'),
+        ('Divorced', 'Divorced')
+    )
+
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
     username = forms.CharField(required=True, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
     email = forms.CharField(required=True)
+    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'mb-2'}), required=True)
+    gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_GENDER, required=True)
+    phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=True)
+    marital_status = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_MARITAL_STATUS, required=True)
+
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
 
-class UpdatePatientsProfileForm(forms.ModelForm):
+class UpdateProfileForm(forms.ModelForm):
     SELECT_GENDER = (
         (None, '-- Select gender --'),
         ('Male', 'Male'),
@@ -27,16 +45,18 @@ class UpdatePatientsProfileForm(forms.ModelForm):
         ('Married', 'Married'),
         ('Divorced', 'Divorced')
     )
+
     dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'mb-2'}), required=True)
     gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_GENDER, required=True)
     phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=True)
     marital_status = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_MARITAL_STATUS, required=True)
 
     class Meta:
-        model = PatientsProfile
+        model = User
         fields = ['dob', 'gender', 'phone_no', 'marital_status', 'profile_pic']
 
-class EditPatientsProfileForm(forms.ModelForm):
+
+class EditProfileForm(forms.ModelForm):
     SELECT_GENDER = (
         (None, '-- Select gender --'),
         ('Male', 'Male'),
@@ -49,67 +69,12 @@ class EditPatientsProfileForm(forms.ModelForm):
         ('Married', 'Married'),
         ('Divorced', 'Divorced')
     )
+
     dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'mb-2'}), disabled=True)
     gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_GENDER, disabled=True)
     phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=True)
     marital_status = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_MARITAL_STATUS, required=True)
 
-    class Meta:
-        model = PatientsProfile
-        fields = ['dob', 'gender', 'phone_no', 'marital_status', 'profile_pic']
-
-
-# custom made forms for therapists
-class TherapistsSignupForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-    email = forms.CharField(required=True)
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
-
-
-
-class UpdateTherapistsProfileForm(forms.ModelForm):
-    SELECT_GENDER = (
-        (None, '-- Select gender --'),
-        ('Male', 'Male'),
-        ('Female', 'Female'),
-    )
-    SELECT_MARITAL_STATUS = (
-        (None, '-- Select your marital status --'),
-        ('Single', 'Single'),
-        ('Engaged', 'Engaged'),
-        ('Married', 'Married'),
-        ('Divorced', 'Divorced')
-    )
-    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'mb-2'}), required=True)
-    gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_GENDER, required=True)
-    phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=True)
-    marital_status = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_MARITAL_STATUS, required=True)
-
-    class Meta:
-        model = TherapistsProfile
-        fields = ['dob', 'gender', 'phone_no', 'marital_status', 'profile_pic']
-
-class EditTherapistsProfileForm(forms.ModelForm):
-    SELECT_GENDER = (
-        (None, '-- Select gender --'),
-        ('Male', 'Male'),
-        ('Female', 'Female'),
-    )
-    SELECT_MARITAL_STATUS = (
-        (None, '-- Select your marital status --'),
-        ('Single', 'Single'),
-        ('Engaged', 'Engaged'),
-        ('Married', 'Married'),
-        ('Divorced', 'Divorced')
-    )
-    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'mb-2'}), disabled=True)
-    gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_GENDER, disabled=True)
-    phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=True)
-    marital_status = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_MARITAL_STATUS, required=True)
-
-    class Meta:
-        model = TherapistsProfile
         fields = ['dob', 'gender', 'phone_no', 'marital_status', 'profile_pic']
