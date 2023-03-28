fa4d64791508f6d349ac945620f529a95d338b15
brocode <kelimbth@gmail.com>
2023-03-28 18:34:01 +0300

    updated homepage URL route names

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homepage/patient/', views.patient_homepage_view, name='patient_homepage'),
+    path('homepage/<str:patient_name>/', views.patient_homepage_view, name='patient_homepage'),
     path('schedule-appointment/', views.schedule_appointments_view, name='schedule_appointment'),
     
-    path('homepage/therapist/', views.therapists_homepage_view, name='therapist_homepage'),
+    path('homepage/<str:therapist_name>/', views.therapists_homepage_view, name='therapist_homepage'),
     path('therapist/add-facility-info/', views.update_facility_info_view, name='medical_facility'),
 
 ]
