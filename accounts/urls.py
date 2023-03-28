d3a527307e5e0188e6249797d795c94a99b97546
brocode <kelimbth@gmail.com>
2023-03-28 12:54:47 +0300

    updated URL route name

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='user_login'),
-    path('create-account/', views.signup_view, name='signup'),
+    path('signup/', views.signup_view, name='signup'),
     path('profile/<str:name>/view-profile/', views.user_profile_view, name='patient_profile'),
 
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
