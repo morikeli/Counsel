35f40edb779f7923d7ea2385d97432687ab1f6fc
brocode <kelimbth@gmail.com>
2023-03-28 16:40:39 +0300

    updated signals func

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,4 @@
 from django.db.models.signals import pre_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import User
 from datetime import datetime
@@ -11,9 +10,7 @@ def generate_userID(sender, instance, **kwargs):
         instance.id = str(uuid.uuid4()).replace('-', '')[:25]
     
     try:
-        if instance.is_superuser is True:
-            pass    # dob is not required for superuser account, thus leave the 'dob' in user model blank when creating superuser a/c.
-        else:
+        if instance.is_therapist is False or instance.is_therapist is True:
             if datetime.now().strftime('%Y-%m-%d %H:%M:%S') > instance.created.strftime('%Y-%m-%d %H:%M:%S'):
                 user_dob = str(instance.dob)
                 get_userDob = datetime.strptime(user_dob, '%Y-%m-%d')
@@ -29,6 +26,9 @@ def generate_userID(sender, instance, **kwargs):
                 user_age = current_date - get_userDob
                 convert_userAge = int(user_age.days/365.25)
                 instance.age = convert_userAge
-            
+        
+        else:
+            pass    # dob is not required for superuser account, thus leave the 'dob' in user model blank when creating superuser a/c.
+
     except AttributeError:
         return
