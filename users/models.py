259c9aa3ba871a05376f15a7f9040c3eb65b75ce
brocode <kelimbth@gmail.com>
2023-03-28 12:42:26 +0300

    fixed errors and made minor changes

--- users/models.py
+++ users/models.py
@@ -1,30 +1,10 @@
+from accounts.models import User
 from django.db import models
-from django.contrib.auth.models import User
-from accounts.models import PatientsProfile, TherapistsProfile
-
-
-class Appointments(models.Model):
-    id = models.CharField(max_length=20, primary_key=True, editable=False, unique=True)
-    name = models.ForeignKey(PatientsProfile, on_delete=models.CASCADE,editable=False)
-    therapist_name = models.CharField(max_length=100, blank=False)
-    appointment_date = models.DateField(blank=False)
-    appointment_time = models.TimeField(blank=False)
-    session = models.CharField(max_length=20, blank=False)
-    approved = models.BooleanField(default=False, editable=False)
-    scheduled = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name_plural = 'Appointments'
-        ordering = ['-scheduled']
-
-    def __str__(self):
-        return f'{self.name}'
 
 
 class Facilities(models.Model):
-    id = models.CharField(max_length=20, primary_key=True, editable=False, unique=True)
-    medic = models.OneToOneField(TherapistsProfile, on_delete=models.CASCADE)
+    id = models.CharField(max_length=25, primary_key=True, editable=False, unique=True)
+    therapist = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
     role = models.CharField(max_length=15, blank=False)
     facility_name = models.CharField(max_length=70, blank=False)
     location = models.CharField(max_length=70, blank=False)
@@ -39,5 +19,24 @@ class Facilities(models.Model):
         ordering = ['facility_name']
 
     def __str__(self):
-        return f'{self.medic}'
+        return self.therapist
+
+
+class Appointments(models.Model):
+    id = models.CharField(max_length=25, primary_key=True, editable=False, unique=True)
+    patient = models.ForeignKey(User, on_delete=models.CASCADE,editable=False)
+    therapist_name = models.CharField(max_length=100, blank=False)
+    appointment_date = models.DateField(blank=False)
+    appointment_time = models.TimeField(blank=False)
+    session = models.CharField(max_length=20, blank=False)
+    approved = models.BooleanField(default=False, editable=False)
+    scheduled = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Appointments'
+        ordering = ['-scheduled']
+
+    def __str__(self):
+        return f'{self.name}'
 
--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,5 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from accounts.models import PatientsProfile, TherapistsProfile
 from .models import Facilities, Appointments
 import uuid
 
