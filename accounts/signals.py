b528d5e1d1ecf1ad1cb31dc55dfc818b1fdf8e9a
brocode <kelimbth@gmail.com>
2023-03-28 15:41:28 +0300

    added an if ... block

--- accounts/signals.py
+++ accounts/signals.py
@@ -11,21 +11,24 @@ def generate_userID(sender, instance, **kwargs):
         instance.id = str(uuid.uuid4()).replace('-', '')[:25]
     
     try:
-        if datetime.now().strftime('%Y-%m-%d %H:%M:%S') > instance.created.strftime('%Y-%m-%d %H:%M:%S'):
-            user_dob = str(instance.dob)
-            get_userDob = datetime.strptime(user_dob, '%Y-%m-%d')
-            current_date = datetime.now()
-            user_age = current_date - get_userDob
-            convert_userAge = int(user_age.days/365.25)
-            instance.age = convert_userAge
-            
+        if instance.is_superuser is True:
+            pass    # dob is not required for superuser account, thus leave the 'dob' in user model blank when creating superuser a/c.
         else:
-            user_dob = str(instance.dob)
-            get_userDob = datetime.strptime(user_dob, '%Y-%m-%d')
-            current_date = datetime.now()
-            user_age = current_date - get_userDob
-            convert_userAge = int(user_age.days/365.25)
-            instance.age = convert_userAge
+            if datetime.now().strftime('%Y-%m-%d %H:%M:%S') > instance.created.strftime('%Y-%m-%d %H:%M:%S'):
+                user_dob = str(instance.dob)
+                get_userDob = datetime.strptime(user_dob, '%Y-%m-%d')
+                current_date = datetime.now()
+                user_age = current_date - get_userDob
+                convert_userAge = int(user_age.days/365.25)
+                instance.age = convert_userAge
+                
+            else:
+                user_dob = str(instance.dob)
+                get_userDob = datetime.strptime(user_dob, '%Y-%m-%d')
+                current_date = datetime.now()
+                user_age = current_date - get_userDob
+                convert_userAge = int(user_age.days/365.25)
+                instance.age = convert_userAge
             
     except AttributeError:
         return
