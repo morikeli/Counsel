3a62edfce8c3fd152019c32aca37700758b1fb39
brocode <kelimbth@gmail.com>
2023-03-28 14:24:50 +0300

    updated SignupForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,7 +17,7 @@ class SignupForm(UserCreationForm):
         ('Divorced', 'Divorced')
     )
 
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), required=True)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2', 'autofocus': 'on'}), required=True)
     last_name = forms.CharField(widget=forms.TextInput(attrs={'type': 'text', 'class': 'mb-2'}), required=True)
     username = forms.CharField(widget=forms.TextInput(attrs={'autocomplete': 'off', 'class': 'mb-2'}), required=True)
     email = forms.CharField(widget=forms.TextInput(attrs={'type': 'email', 'class': 'mb-2'}), required=True)
@@ -25,11 +25,12 @@ class SignupForm(UserCreationForm):
     gender = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_GENDER, required=True)
     phone_no = forms.CharField(widget=forms.TextInput(attrs={'type': 'tel', 'class': 'mb-2'}), required=True)
     marital_status = forms.ChoiceField(widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}), choices=SELECT_MARITAL_STATUS, required=True)
+    is_therapist = forms.BooleanField(widget=forms.CheckboxInput(attrs={'type': 'checkbox', 'class': 'mt-2 mb-3'}), label='I am a therapist', required=True, help_text='Are you a certified/trained therapist, counsellor or a psychiatrist?')
 
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'dob', 'gender', 'phone_no', 'marital_status', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'dob', 'gender', 'phone_no', 'marital_status', 'password1', 'password2', 'is_therapist']
 
 
 class UpdateProfileForm(forms.ModelForm):
