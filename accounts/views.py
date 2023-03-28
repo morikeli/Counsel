bf6de7efe856c7b0d68f31136f7809501ad471df
brocode <kelimbth@gmail.com>
2023-03-28 12:33:29 +0300

    updated views

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,11 @@
-from .forms import (PatientsSignupForm, TherapistsSignupForm, UpdatePatientsProfileForm,
-    UpdateTherapistsProfileForm, EditPatientsProfileForm, EditTherapistsProfileForm)
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.views import LogoutView
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import PatientsProfile, TherapistsProfile
+from .forms import SignupForm, UpdateProfileForm, EditProfileForm
+from .models import User
 
 
 def login_view(request):
@@ -24,18 +22,18 @@ def login_view(request):
             
             if user_account is not None:
                 if user_account.is_staff is True:
-                    if user_account.therapistsprofile is True and user_account.therapistsprofile.is_therapist is True:
+                    if user_account.is_therapist is True:
                         auth.login(request, user_account)
-                        return redirect('therapist_profile', user_account.therapistsprofile.medic)
+                        return redirect('profile')
                     
                     else:
                         auth.login(request, user_account)
                         return redirect('therapist_homepage')
                 
                 elif user_account.is_staff is False:
-                    if  user_account.is_staff is False and user_account.patientsprofile.is_patient is False:
+                    if  user_account.is_patient is True:
                         auth.login(request, user_account)
-                        return redirect('patient_profile', user_account.patientsprofile.patient)
+                        return redirect('profile')
 
                     else:
                         auth.login(request, user_account)
@@ -51,50 +49,31 @@ def login_view(request):
 
 
 def signup_view(request):
-    form = PatientsSignupForm()
+    form = SignupForm()
 
     if request.method == 'POST':
-        form = PatientsSignupForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
-            # this code fetches the name of the patient. The name is used as a parameter when redirecting a user to his/her homepage.
-            new_patient_account = form.save(commit=False)
-            new_patient_account.save()
+            new_user = form.save(commit=False)
+            new_user.save()
 
             messages.success(request, f'Account created successfully!')
-            return redirect('patient_profile', new_patient_account.patientsprofile.patient)
+            return redirect('patient_profile', new_user.username)
 
     context = {'signup_form': form}
     return render(request, 'patients/signup.html', context)
 
 
-def counsellor_signup_view(request):
-    form = CounsellorsSignupForm()
-
-    if request.method == 'POST':
-        form = CounsellorsSignupForm(request.POST)
-
-        if form.is_valid():
-            new_therapist_account = form.save(commit=False)
-            new_therapist_account.is_staff = True
-            new_therapist_account.save()
-
-            messages.success(request, f'Account for {new_therapist_account.username} created successfully!')
-            return redirect('therapist_profile')
-
-
-    context = {'therapist_signup_form': form}
-    return render(request, 'counsellors/counsellors-signup.html', context)
-
 @login_required(login_url='user_login')
 @user_passes_test(lambda user: user.is_staff is False and user.is_superuser is False)
-def patientsprofile_view(request, patient_name):
-    current_patient = PatientsProfile.objects.get(patient__username=patient_name)     # current patient request
-    updateprofile_form = UpdatePatientsProfileForm(instance=request.user.patientsprofile)
-    editprofile_form = EditPatientsProfileForm(instance=request.user.patientsprofile)
+def user_profile_view(request, name):
+    user_obj = User.objects.get(username=name)
+    updateprofile_form = UpdateProfileForm(instance=user_obj)
+    editprofile_form = EditProfileForm(instance=user_obj)
 
     if request.method == 'POST':
-        updateprofile_form = UpdatePatientsProfileForm(request.POST, request.FILES, instance=request.user.patientsprofile)
-        editprofile_form = EditPatientsProfileForm(request.POST, request.FILES, instance=request.user.patientsprofile)
+        updateprofile_form = UpdateProfileForm(request.POST, request.FILES, instance=user_obj)
+        editprofile_form = EditProfileForm(request.POST, request.FILES, instance=user_obj)
 
         if updateprofile_form.is_valid():
             new_patient = updateprofile_form.save(commit=False)
@@ -102,44 +81,16 @@ def patientsprofile_view(request, patient_name):
             new_patient.save()
 
             messages.success(request, 'Your profile was updated successfully!')
-            return redirect('patient_profile', patient_name)
+            return redirect('profile', name)
 
         elif editprofile_form.is_valid():
             editprofile_form.save()
 
             messages.info(request, 'Profile edited successfully!')
-            return redirect('patient_profile', patient_name)
+            return redirect('profile', name)
 
     context = {'UpdateProfileForm': updateprofile_form, 'EditProfileForm': editprofile_form}
     return render(request, 'patients/profile.html', context)
-
-@login_required(login_url='user_login')
-@user_passes_test(lambda user: user.is_staff is True and user.is_superuser is False)
-def therapistprofile_view(request, medic_name):
-    current_therapist = TherapistsProfile.objects.get(counsellor__username=medic_name)    # get a logged in therapist request
-    updateprofile_form = UpdateTherapistsProfileForm(instance=request.user.therapistsprofile)
-    editprofile_form = EditTherapistsProfileForm(instance=request.user.therapistsprofile)
-
-    if request.method == 'POST':
-        updateprofile_form = UpdateTherapistsProfileForm(request.POST, request.FILES, instance=request.user.therapistsprofile)
-        editprofile_form = EditTherapistsProfileForm(request.POST, request.FILES, instance=request.user.therapistsprofile)
-
-        if updateprofile_form.is_valid():
-            new_therapist = updateprofile_form.save(commit=False)
-            new_therapist.is_therapist = True
-            new_therapist.save()
-
-            messages.success(request, 'Your profile was updated successfully!')
-            return redirect('therapist_profile', medic_name)
-
-        elif editprofile_form.is_valid():
-            editprofile_form.save()
-
-            messages.info(request, 'Profile edited successfully!')
-            return redirect('therapist_profile', medic_name)
-
-    context = {'UpdateProfileForm': updateprofile_form, 'EditProfileForm': editprofile_form}
-    return render(request, 'counsellors/profile.html', context)
 
 
 class LogoutUserView(LogoutView):
