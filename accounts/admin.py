6d720fc777619fec3473912d356291012fde2877
brocode <kelimbth@gmail.com>
2023-03-28 12:37:43 +0300

    fixed runtime errors

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,7 +7,7 @@ from .models import User
 class UsersLayout(UserAdmin):
     model = User
     add_form = SignupForm
-    list_display = ['patient', 'gender', 'marital_status', 'is_patient']
-    readonly_fields = ['patient', 'gender', 'phone_no', 'dob', 'marital_status', 'is_patient', 'profile_pic']
+    list_display = ['username', 'gender', 'marital_status', 'is_patient']
+    readonly_fields = ['gender', 'phone_no', 'dob', 'marital_status', 'is_patient', 'profile_pic']
 
 admin.site.register(User, UsersLayout)
