b104ee31679504165c1974646db140895394ea5e
brocode <kelimbth@gmail.com>
2023-03-31 13:41:40 +0300

    created new table for testimonials

--- users/models.py
+++ users/models.py
@@ -41,3 +41,15 @@ class Appointments(models.Model):
     def __str__(self):
         return self.patient
 
+
+class Testimonials(models.Model):
+    id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    message = models.TextField(blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user
+    
+    
