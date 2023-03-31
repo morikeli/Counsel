8e2ce3a5fc2908eb23c9ea6c80d3a69f675ac5dc
brocode <kelimbth@gmail.com>
2023-03-31 14:52:52 +0300

    minor change

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('homepage/<str:patient_name>/', views.patient_homepage_view, name='patient_homepage'),
     path('schedule-appointment/', views.available_therapists_view, name='therapists'),
-    path('<str:name>/sessions', views.sessions_view, name='sessions'),
+    path('<str:name>/sessions/', views.sessions_view, name='sessions'),
     path('blogs/', views.blogs_view, name='blogs'),
     
     path('homepage/<str:therapist_name>/', views.therapists_homepage_view, name='therapist_homepage'),
