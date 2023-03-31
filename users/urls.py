f1356dd113db67656379b3a39d964d3b33b6d37f
brocode <kelimbth@gmail.com>
2023-03-31 14:36:44 +0300

    new URL app

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('homepage/<str:patient_name>/', views.patient_homepage_view, name='patient_homepage'),
-    path('schedule-appointment/', views.schedule_appointments_view, name='schedule_appointment'),
+    path('schedule-appointment/', views.available_therapists_view, name='therapists'),
+    path('<str:name>/sessions', views.sessions_view, name='sessions'),
+    path('blogs/', views.blogs_view, name='blogs'),
     
     path('homepage/<str:therapist_name>/', views.therapists_homepage_view, name='therapist_homepage'),
     path('therapist/add-facility-info/', views.update_facility_info_view, name='medical_facility'),
