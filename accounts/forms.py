b483ecf1f2516ae04a00446cc4aa80f9a2efaf97
brocode <kelimbth@gmail.com>
2023-10-22 13:40:03 +0300

    created signup & editprofile form

---
+++ accounts/forms.py
@@ -0,0 +1,103 @@
+from django.contrib.auth.forms import UserCreationForm
+from .utils import validate_image_file
+from .models import User
+from django import forms
+
+
+class SignupForm(UserCreationForm):
+    SELECT_GENDER = (
+        (None, '-- Select your gender --'),
+        ('Male', 'Male'),
+        ('Female', 'Female')
+    )
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'text', 'class': 'mb-2', 'autofocus': True
+        }),
+        required=True,
+    )
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+           'type': 'text', 'class': 'mb-2',
+        }),
+        required=True,
+    )
+    username = forms.CharField(widget=forms.TextInput(attrs={
+           'type': 'text', 'class': 'mb-2',
+        }),
+        required=True,
+    )
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'type': 'email', 'class': 'mb-2',
+        }),
+    )
+    mobile_no = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'tel', 'class': 'mb-0',
+        }),
+        help_text='Enter your phone number and include your country code, e.g. +254112345678'
+    )
+    gender = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        choices=SELECT_GENDER,
+    )
+    dob = forms.DateField(widget=forms.DateInput(attrs={
+            'type': 'date', 'class': 'mb-2',
+        }),
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'gender', 'dob', 'mobile_no', 'password1', 'password2']
+
+class EditProfileForm(forms.ModelForm):
+    SELECT_GENDER = (
+        (None, '-- Select your gender --'),
+        ('Male', 'Male'),
+        ('Female', 'Female')
+    )
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'text', 'class': 'mb-2', 'autofocus': True
+        }),
+        required=True,
+        disabled=True,
+    )
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+           'type': 'text', 'class': 'mb-2',
+        }),
+        required=True,
+        disabled=True,
+    )
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'type': 'email', 'class': 'mb-2',
+        }),
+    )
+    gender = forms.ChoiceField(widget=forms.Select(attrs={
+            'type': 'select', 'class': 'mb-2',
+        }),
+        choices=SELECT_GENDER,
+        disabled=True,
+    )
+    dob = forms.DateField(widget=forms.DateInput(attrs={
+            'type': 'date', 'class': 'mb-2',
+        }),
+        required=True,
+        disabled=True,
+    )
+    mobile_no = forms.CharField(widget=forms.TextInput(attrs={
+            'type': 'tel', 'class': 'mb-0',
+        }),
+        help_text='Enter your phone number and include your country code, e.g. +254112345678'
+    )
+    profile_pic = forms.FileField(
+        widget=forms.FileInput(attrs={
+            'type': 'file', 'class': 'form-control mb-2', 'accept': '.jpg, .jpeg, .png',
+        }),
+        required=False,
+        validators=[validate_image_file],
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'gender', 'dob', 'mobile_no', 'profile_pic']
